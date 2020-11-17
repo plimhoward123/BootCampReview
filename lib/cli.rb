20f0a4784e0633d272ec67581d68c089899f7342
Plimon Howard <plimon.howard@gmail.com>
2020-11-17 06:08:54 +0000

    cleaned up output text in cli class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,17 +1,19 @@
 class CLI
   
   def initialize
-    self.intro
+    intro
   end
   
   def intro
-    #Introduction instructions for simple input
-    puts " Enter 1-35 to expand information on the Boot Camp "
-    puts "              Press 0 to exit                      "
-    puts "---------------------------------------------------"
-    
+
     #Initialize Scrape class to gather data from webpage and create a new object of it self, and store it into a class variable @@all.
     scrape = Scrape.new.scrape_data
+    
+    #Introduction instructions for simple input
+    puts "                                                                      "
+    puts " Enter 1 - #{School.all.length} to expand information on the Boot Camp"
+    puts "              Press 0 to exit                      "
+    puts "---------------------------------------------------"
     
     #Iterate through each instance of a school object with index
     School.all.each_with_index do |obj, i|
@@ -23,12 +25,14 @@ class CLI
     
     #gets user input 
     while (input = gets.chomp.to_i) != 0
-      if(input > 1 && input < 35)
+      if(input > 1 && input < 35 && input.is_a?(Integer))
         tmpObj = School.all[input-1]
         puts "Name        : " + tmpObj.name
         puts "Rating      : " + tmpObj.rating
         puts "Locations   : " + tmpObj.locations
         puts "Description : " + tmpObj.description + "\n\n"
+      else
+       puts "Please enter 1-#{School.all.length}"
       end
     end
   end
